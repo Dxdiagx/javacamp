1024992e9ba5c87f8a6743b41356d0feb96be634
Samet Acar <40319021+Dxdiagx@users.noreply.github.com>
2021-04-29 03:30:55 +0300

    Add files via upload

---
+++ javaode2/src/com/company/Category.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Category {
+int id;
+String name;
+
+
+
+}
---
+++ javaode2/src/com/company/RollCall.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class RollCall {
+    void rollcall(Person person){
+
+        System.out.println("Yoklamanız Alındı."+ "\n" +"Derse devam edebilirsiniz."+"\n"
+                +person.name+" "+
+                person.surname);
+
+
+    }
+
+}
