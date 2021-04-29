53272a9b6d975ae3c2ddfdca08b35aef8d093cea
Samet Acar <40319021+Dxdiagx@users.noreply.github.com>
2021-04-29 03:45:36 +0300

    Add files via upload

---
+++ javaodev2/src/com/company/Feature.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Feature {
+    String name;
+    int id;
+    String detail;
+
+
+     public Feature(String name,int id,String detail){
+
+       this.name=name;
+       this.id=id;
+       this.detail=detail;
+
+
+     }
+
+}
