e5e53efb6a9ac1b20ced80e725fddef79a2102e6
Samet Acar <40319021+Dxdiagx@users.noreply.github.com>
2021-05-26 15:02:47 +0300

    Delete hrms directory

--- hrms/src/main/java/com/example/hrms/dataAccess/abstracts/JobPositionDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.hrms.dataAccess.abstracts;
-
-import com.example.hrms.entities.concretes.JobPositions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPositionDao extends JpaRepository<JobPositions,Integer> {
-}
